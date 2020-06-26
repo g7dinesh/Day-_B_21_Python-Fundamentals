b37035977480b7a2a8bce689cbdecfb8b8e3e58f
g7dinesh <67426132+g7dinesh@users.noreply.github.com>
2020-06-26 20:38:28 +0530

    Add files via upload
    Day 3 Practice session

---
+++ Day3_B_21_Python Fundamentals.py
@@ -0,0 +1,97 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Introduction to f String
+
+
+# In[6]:
+
+
+first_name = 'dinesh'
+last_name = 'gudimetla'
+full_name = f"{first_name} {last_name}"
+print(full_name)
+
+
+# In[ ]:
+
+
+Enhancement of the code
+
+
+# In[7]:
+
+
+print(full_name.title())
+
+
+# In[26]:
+
+
+print(f"keep up the good work, {full_name.title()}")
+
+
+# In[27]:
+
+
+print('python')
+
+
+# In[28]:
+
+
+print('\tpython')
+
+
+# In[29]:
+
+
+print('fav_launguages: \npython\njava\nc++')
+
+
+# In[30]:
+
+
+print('fav_launguages: \n\tpython\n\tjava\n\tc++')
+
+
+# In[ ]:
+
+
+Removing white spaces
+
+
+# In[31]:
+
+
+fav_launguage = ' python'
+print(fav_launguage)
+
+
+# In[32]:
+
+
+print(fav_launguage.lstrip())
+
+
+# In[38]:
+
+
+fav_launguage1 = 'C++  '
+print(fav_launguage1)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
