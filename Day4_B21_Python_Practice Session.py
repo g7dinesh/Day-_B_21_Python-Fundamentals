46bdddb6cafba9ef3e811241f62317d1b2f7214a
g7dinesh <67426132+g7dinesh@users.noreply.github.com>
2020-06-29 19:50:19 +0530

    Add files via upload

---
+++ Day4_B21_Python_Practice Session.py
@@ -0,0 +1,92 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[ ]:
+
+
+Introduction to List Data Types
+
+
+# In[4]:
+
+
+Employees =['sai','dinesh','ravi','rohit']
+type(Employees)
+
+
+# In[5]:
+
+
+print(Employees[0])
+
+
+# In[7]:
+
+
+my_Employees = ['sai','dinesh','ravi','rohit']
+print(my_Employees)
+
+
+# In[10]:
+
+
+my_Employees[1] = 'chinni'
+
+
+# In[11]:
+
+
+print(my_Employees)
+
+
+# In[13]:
+
+
+my_Employees.append('chandu')
+print(my_Employees)
+
+
+# In[25]:
+
+
+my_Employees.insert[2,'vamsi']
+print(my_Employees)
+
+
+# In[28]:
+
+
+del my_Employees[2]
+print(my_Employees)
+
+
+# In[29]:
+
+
+x = my_Employees.pop()
+print(x)
+
+
+# In[30]:
+
+
+print(my_Employees)
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
