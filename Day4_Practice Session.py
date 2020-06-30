9355a138d8973bf0601313b6c753dec7434846d5
g7dinesh <67426132+g7dinesh@users.noreply.github.com>
2020-06-30 20:00:51 +0530

    Example of For Loop
    Example of For Loop

---
+++ Day4_Practice Session.py
@@ -0,0 +1,150 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+Employees =['sai','dinesh','ravi','rohit']
+Employees.insert(2,"chandu")
+print(Employees)
+
+
+# my_Employees = ['sai','dinesh','ravi','rohit']
+
+# In[6]:
+
+
+my_Employees = ['sai','dinesh','ravi','rohit']
+my_Employees.insert(3,"chinni")
+print(my_Employees)
+
+
+# In[ ]:
+
+
+Organising Lists:
+
+
+# In[7]:
+
+
+cards = ['hdfc','amex','axis','indusind','sbi','scb']
+
+
+# In[8]:
+
+
+print(cards)
+
+
+# In[9]:
+
+
+type(cards)
+
+
+# In[10]:
+
+
+print(sorted(cards))
+
+
+# In[11]:
+
+
+print(cards)
+
+
+# In[12]:
+
+
+cards.sort()
+print(cards)
+
+
+# In[13]:
+
+
+print(cards)
+
+
+# In[14]:
+
+
+cards.reverse()
+print(cards)
+
+
+# In[15]:
+
+
+print(cards)
+
+
+# In[16]:
+
+
+len(cards)
+
+
+# In[ ]:
+
+
+Introduction to For loop:
+
+
+# In[17]:
+
+
+Employees = ['sai','ramya','naresh','chandra','mounika','mani','naidu']
+
+
+# In[21]:
+
+
+print(Employees)
+
+
+# In[25]:
+
+
+for Emp in Employees:
+    print(f "keep up the good work ,{Employees.title()}")
+
+
+# In[27]:
+
+
+print(Employees)
+
+
+# In[33]:
+
+
+for Emp in Employees:
+    print(f "keep up the good work , {Employees.title()}")
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
